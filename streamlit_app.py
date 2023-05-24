7b33473c46ce7e6d77e5e902d9296f9ad3dc4f9a
cliffordkaosei <132464285+cliffordkaosei@users.noreply.github.com>
2023-05-24 17:24:53 +0100

    Update streamlit_app.py
    added filtered dataframe of selected fruits
    
    display new dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,9 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
+# Put selected fruit list into dataframe
+
+fruits_to_show = streamlit.dataframe(fruits_selected)
+
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
